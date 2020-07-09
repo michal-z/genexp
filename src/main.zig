e7984ae806144e3b9737109acfa13e341f013ed6
michal-z <michalziulek@gmail.com>
2020-07-09 23:25:10 +0200

    -

--- src/main.zig
+++ src/main.zig
@@ -9,56 +9,98 @@ fn errorCallback(err: c_int, description: [*c]const u8) callconv(.C) void {
     panic("Error: {}\n", .{@as([*:0]const u8, description)});
 }
 
-fn graphicsErrorCallback(source: c.GLenum, stype: c.GLenum, id: c.GLuint, severity: c.GLenum, length: c.GLsizei, message: [*c]const u8, param: ?*const c_void) callconv(.C) void {
+fn graphicsErrorCallback(
+    source: c.GLenum,
+    stype: c.GLenum,
+    id: c.GLuint,
+    severity: c.GLenum,
+    length: c.GLsizei,
+    message: [*c]const u8,
+    param: ?*const c_void,
+) callconv(.C) void {
     if (message != null) {
         warn("{}\n", .{@as([*:0]const u8, message)});
     }
 }
 
-const FpsStats = struct {
-    timer: std.time.Timer,
-    previous_time_ns: u64,
-    header_refresh_time_ns: u64,
-    frame_count: u64,
-    time: f64,
-    delta_time: f32,
+fn returnData() struct { time: f64, delta_time: f32 } {
+    var time: f64 = 1.0;
+    var delta_time: f32 = 2.0;
+    return .{ .time = time, .delta_time = delta_time };
+}
 
-    fn init() FpsStats {
-        const timer = std.time.Timer.start() catch unreachable;
-        const ns = timer.read();
-        return FpsStats{
-            .timer = timer,
-            .previous_time_ns = ns,
-            .header_refresh_time_ns = ns,
-            .frame_count = 0,
-            .time = 0.0,
-            .delta_time = 0.0,
-        };
-    }
+const Vec3 = struct {
+    x: f32,
+    y: f32,
+    z: f32,
 
-    fn update(self: *FpsStats, window: ?*c.GLFWwindow, name: []const u8) void {
-        const now_ns = self.timer.read();
-
-        self.time = @intToFloat(f64, now_ns) / std.time.ns_per_s;
-        self.delta_time = @intToFloat(f32, now_ns - self.previous_time_ns) / std.time.ns_per_s;
-        self.previous_time_ns = now_ns;
-
-        if ((now_ns - self.header_refresh_time_ns) >= std.time.ns_per_s) {
-            const t = @intToFloat(f64, now_ns - self.header_refresh_time_ns) / std.time.ns_per_s;
-            const fps = @intToFloat(f64, self.frame_count) / t;
-            const ms = (1.0 / fps) * 1000.0;
-
-            var buffer = [_]u8{0} ** 256;
-            const buffer_slice = buffer[0..];
-            const header = std.fmt.bufPrint(buffer_slice, "[{d:.1} fps  {d:.3} ms] {}", .{ fps, ms, name }) catch unreachable;
-            c.glfwSetWindowTitle(window, &header[0]);
-
-            self.header_refresh_time_ns = now_ns;
-            self.frame_count = 0;
-        }
-        self.frame_count += 1;
+    pub fn add(self: *Vec3, other: Vec3) Vec3 {
+        self.x += other.x;
+        self.y += other.y;
+        self.z += other.z;
+        return self.*;
     }
 };
+
+test "vec3" {
+    var v0 = Vec3{
+        .x = 1.0,
+        .y = 2.0,
+        .z = 3.0,
+    };
+    var v1 = Vec3{
+        .x = 4.0,
+        .y = 5.0,
+        .z = 6.0,
+    };
+    const v3 = v0.add(v1).add(v1);
+    const r = returnData();
+    //assert(v0.x == 9.0 and v3.x == 9.0);
+    assert(r.time == 1.0 and r.delta_time == 2.0);
+}
+
+fn updateFrameStats(window: *c.GLFWwindow, name: [*:0]const u8) struct { time: f64, delta_time: f32 } {
+    const state = struct {
+        var timer: std.time.Timer = undefined;
+        var previous_time_ns: u64 = 0;
+        var header_refresh_time_ns: u64 = 0;
+        var frame_count: u64 = ~@as(u64, 0);
+    };
+
+    if (state.frame_count == ~@as(u64, 0)) {
+        state.timer = std.time.Timer.start() catch unreachable;
+        state.previous_time_ns = 0;
+        state.header_refresh_time_ns = 0;
+        state.frame_count = 0;
+    }
+
+    const now_ns = state.timer.read();
+    const time = @intToFloat(f64, now_ns) / std.time.ns_per_s;
+    const delta_time = @intToFloat(f32, now_ns - state.previous_time_ns) / std.time.ns_per_s;
+    state.previous_time_ns = now_ns;
+
+    if ((now_ns - state.header_refresh_time_ns) >= std.time.ns_per_s) {
+        const t = @intToFloat(f64, now_ns - state.header_refresh_time_ns) / std.time.ns_per_s;
+        const fps = @intToFloat(f64, state.frame_count) / t;
+        const ms = (1.0 / fps) * 1000.0;
+
+        var buffer = [_]u8{0} ** 128;
+        const buffer_slice = buffer[0 .. buffer.len - 1];
+        const header = std.fmt.bufPrint(
+            buffer_slice,
+            "[{d:.1} fps  {d:.3} ms] {}",
+            .{ fps, ms, name },
+        ) catch buffer_slice;
+
+        c.glfwSetWindowTitle(window, header.ptr);
+
+        state.header_refresh_time_ns = now_ns;
+        state.frame_count = 0;
+    }
+    state.frame_count += 1;
+
+    return .{ .time = time, .delta_time = delta_time };
+}
 
 pub fn main() anyerror!void {
     _ = c.glfwSetErrorCallback(errorCallback);
@@ -66,8 +108,6 @@ pub fn main() anyerror!void {
         panic("Failed to init GLFW.\n", .{});
     }
     defer c.glfwTerminate();
-
-    var fps_stats = FpsStats.init();
 
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 4);
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 6);
@@ -109,7 +149,7 @@ pub fn main() anyerror!void {
     c.glNamedFramebufferTexture(fbo, c.GL_COLOR_ATTACHMENT0, fbo_texture, 0);
 
     while (c.glfwWindowShouldClose(window) == c.GLFW_FALSE) {
-        fps_stats.update(window, "genexp");
+        const stats = updateFrameStats(window, "genexp");
 
         c.glBindFramebuffer(c.GL_DRAW_FRAMEBUFFER, fbo);
         c.glClearBufferfv(c.GL_COLOR, 0, &[4]f32{ 1.0, 1.0, 1.0, 1.0 });
