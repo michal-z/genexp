583f02ecd643ce96a39eb4f93da125e42367aced
Michal Ziulek <michalziulek@gmail.com>
2020-08-11 18:50:06 +0200

    -

--- src/genexp001.zig
+++ src/genexp001.zig
@@ -1,7 +1,6 @@
-const builtin = @import("builtin");
 const std = @import("std");
-const c = @import("c.zig");
 const math = std.math;
+const gl = @import("opengl.zig");
 usingnamespace @import("util.zig");
 
 pub const window_name = "genexp001";
@@ -23,9 +22,9 @@ pub const GenerativeExperimentState = struct {
 };
 
 pub fn setup(genexp: *GenerativeExperimentState) !void {
-    c.glPointSize(7.0);
-    c.glEnable(c.GL_BLEND);
-    c.glBlendFunc(c.GL_ONE, c.GL_ONE);
+    gl.pointSize(7.0);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.ONE, gl.ONE);
 }
 
 pub fn update(genexp: *GenerativeExperimentState, time: f64, dt: f32) void {
@@ -65,12 +64,12 @@ pub fn update(genexp: *GenerativeExperimentState, time: f64, dt: f32) void {
 
     const rc = genexp.prng.random.uintLessThan(u8, 3);
     switch (rc) {
-        0 => c.glColor4f(0.5, 0.0, 0.0, 1.0),
-        1 => c.glColor4f(0.0, 0.5, 0.0, 1.0),
-        2 => c.glColor4f(0.0, 0.0, 0.5, 1.0),
+        0 => gl.color4f(0.5, 0.0, 0.0, 1.0),
+        1 => gl.color4f(0.0, 0.5, 0.0, 1.0),
+        2 => gl.color4f(0.0, 0.0, 0.5, 1.0),
         else => unreachable,
     }
-    c.glBegin(c.GL_POINTS);
-    c.glVertex2f(position.x, position.y);
-    c.glEnd();
+    gl.begin(gl.POINTS);
+    gl.vertex2f(position.x, position.y);
+    gl.end();
 }
